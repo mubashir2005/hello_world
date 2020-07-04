7c94123f0dd4f4ec0ef687331af7f5c16b64743a
Mubashir45191 <mubashirhasan716@gmail.com>
2020-07-04 20:29:13 +0530

    git commit all

---
+++ android/app/src/main/java/hasantech/hello_world/MainActivity.java
@@ -0,0 +1,6 @@
+package hasantech.hello_world;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+// Android Studio is the best ide for Flutter Development.
+//The best ide for web development is  Jetbrains Webstorm.
+//Vs Code is the best code editor for everything.
+
+// Main is the starting point for all our apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
+// Everything is flutter is a widget. Flutter has prebuilt widgets which combine to make a beautiful and an interactive app.
+// Our flutter app is basically a widget tree.
